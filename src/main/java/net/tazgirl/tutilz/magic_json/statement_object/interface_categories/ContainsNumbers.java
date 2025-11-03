5bd9a6afbeb38c96361d0259ae56fac50e2136fa
TM316157 <tallenmcgill@gmail.com>
2025-11-03 01:04:14 +0000

    Moved MagicJSON to it's own project

--- src/main/java/net/tazgirl/tutilz/magic_json/statement_object/interface_categories/ContainsNumbers.java
+++
@@ -1,5 +0,0 @@
-package net.tazgirl.tutilz.magic_json.statement_object.interface_categories;
-
-public interface ContainsNumbers
-{
-}
--- src/main/java/net/tazgirl/tutilz/magic_json/statement_object/interface_categories/ResolvesBoolean.java
+++
@@ -1,9 +0,0 @@
-package net.tazgirl.tutilz.magic_json.statement_object.interface_categories;
-
-import java.util.Map;
-
-public interface ResolvesBoolean
-{
-    public Boolean Resolve();
-    public void SpreadArgs(Map<String, Object> newArgs);
-}
